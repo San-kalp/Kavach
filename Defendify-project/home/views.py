4df1189316c1405cd5e02c93257843036cc9374f
San-kalp <sankalp.chordia20@vit.edu>
2023-07-22 18:03:18 +0530

    Changes

--- Defendify-project/home/views.py
+++ Defendify-project/home/views.py
@@ -8,8 +8,11 @@ from .forms import addressForm
 from django.http import HttpResponseRedirect
 import requests
 import json
+import datetime
+import re
 
 renderer = JSONRenderer()
+ETHERSCAN_API_KEY = "2SFM3DIEINQ9Z7B27U9V4C4ICHUHR25R9A"
 
 
 def home (request):
@@ -18,21 +21,25 @@ def home (request):
         form = addressForm(request.POST)
         if form.is_valid():
             address = form.cleaned_data['address']
-            url = "https://blockchain.info/rawaddr/%s" %address
-            unspent = requests.get('https://blockchain.info/unspent?active=%s' % address).json()
-            response = requests.get(url)
-            data = json.loads(response.text)
-            address_ = data["address"]
-            amountReceived = data["total_received"]
-            amountSent = data["total_sent"]
-            balance = data["final_balance"]
-            number_of_transactions = data["n_tx"]
-            transaction = data["txs"]
-            # balance = (data[address]['final_balance'])/100000000
-            # n_tx = data[address]['n_tx']
-            # total_received = (data[address]['total_received'])/100000000
-            context = [address_,amountReceived,amountSent,balance,number_of_transactions,transaction,unspent]
-            return render(request,"home/addressInfo.html",{'response':context})
+            if re.match(r"^(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}$",address) :
+                url = "https://blockchain.info/rawaddr/%s" %address
+                unspent = requests.get('https://blockchain.info/unspent?active=%s' % address).json()
+                response = requests.get(url)
+                data = json.loads(response.text)
+                address_ = data["address"]
+                amountReceived = data["total_received"]
+                amountSent = data["total_sent"]
+                balance = data["final_balance"]
+                number_of_transactions = data["n_tx"]
+                transaction = data["txs"]
+                context = [address_,amountReceived,amountSent,balance,number_of_transactions,transaction,unspent]
+                return render(request,"home/addressInfo.html",{'response':context})
+            elif re.match(r"^0x[a-fA-F0-9]{40}$",address):
+                url = f"https://api.etherscan.io/api?module=account&action=balance&address={address}&tag=latest&apikey={ETHERSCAN_API_KEY}"
+                response = requests.get(url)
+                data = json.loads(response.text)
+                print(data)
+
 
 
 
