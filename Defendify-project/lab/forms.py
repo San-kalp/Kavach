f609794e578506dccfacb32b4d8e9759c6485677
San-kalp <sankalp.chordia20@vit.edu>
2023-08-06 18:25:14 +0530

    Added lab features

--- Defendify-project/lab/forms.py
+++ Defendify-project/lab/forms.py
@@ -6,4 +6,3 @@ from django import forms
 class searchForm(forms.Form):
     form_data = forms.CharField(label="Search" , max_length=100)
 
-
--- Defendify-project/lab/views.py
+++ Defendify-project/lab/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import searchForm
 import json
 from neo4j import GraphDatabase
 from pyvis.network import Network
 from django.conf import settings
-
+import pandas as pd
+import Cypher as c
 
 
 
@@ -15,7 +16,7 @@ password = "12345678"
 driver = GraphDatabase.driver(uri, auth=(user, password))
 session = driver.session(database="test")
 
-# Create your views here.
+
 
 def home(request):
     form = searchForm()
@@ -24,89 +25,121 @@ def home(request):
         if form.is_valid():
             input_data = form.cleaned_data['form_data']
             if input_data == '12t9YDPgwueZ9NyMgw519p7AA8isjr6SMw':
-                   query = 'MATCH p=()-[r:PAYS]->() RETURN p LIMIT 25'
-                   result = session.run(query)
-                   data = result.data()
-                   net = Network()
-                   for item in data:
-                          source_index = item['p'][0]['index']
-                          target_index = item['p'][2]['index']
+                 return redirect('wannacry')
+            
+               #     query = 'MATCH p=()-[r:PAYS]->() RETURN p LIMIT 25'
+               #     result = session.run(query)
+               #     data = result.data()
+               #     net = Network(height="500px", width="100%")
+               #     for item in data:
+               #            source_index = item['p'][0]['index']
+               #            target_index = item['p'][2]['index']
                                    
-                          net.add_node(source_index)
-                          net.add_node(target_index)
-                          net.add_edge(source_index, target_index, label='PAYS') 
-                   net.barnes_hut()
-                   net.save_graph(str(settings.BASE_DIR)+'/lab/templates/pvis_graph_file.html')
-            return render(request, 'lab/ss.html')
+               #            net.add_node(source_index, color="#00FF7F")
+               #            net.add_node(target_index)
+               #            net.add_edge(source_index, target_index, label='PAYS',arrows='to') 
+               # #     net.barnes_hut()
+               #     net.save_graph(str(settings.BASE_DIR)+'/lab/templates/pvis_graph_file.html')
 
-
+               #     df = pd.json_normalize(data) 
+               #     filename = "12t9YDPgwueZ9NyMgw519p7AA8isjr6SMw_txs_outs.json"
+              
+                           
+              
      
     context = {'form':form}
     return render(request,"lab/index.html",context=context)
-#     query = 'MATCH (n) RETURN COUNT(n) AS count'
-#     result = session.run(query)
-#     return HttpResponse(result.data())
 
 
-# def wannaCry(address):
-#     query = 'MATCH p=()-[r:PAYS]->() RETURN p LIMIT 25'
-#     result = session.run(query)
-#     data = result.data()
-#     map_data(data)
+def show_data1(request):
+      query = 'MATCH p=()-[r:PAYS]->() RETURN p LIMIT 25'
+      result = session.run(query)
+      data = result.data()
+      df = pd.json_normalize(data)
+      html = df.to_html()
 
-    
-
-
-    
+      context = {'html':html}
+      return render(request,"lab/show_data1.html",context=context)
 
 
 
+def wannacry_function(request):
+      if request.method == "POST":
+           return redirect("create_graph_1")
+      return render(request,"lab/wannacry/wannacry.html")
 
 
+def create_graph_1(request):
+     c.create_graph_wannacry()
+     c.update_depth()
+     c.cast_depth()
+     c.parse_timestamp()
+     c.parse_tx()
+     c.format_timestamp()
+     c.format_tx()
+     query = 'MATCH p=()-[r:PAYS]->() RETURN p LIMIT 25'
+     result = session.run(query)
+     data = result.data()
+     net = Network(height="500px", width="100%")
+     for item in data:
+          source_index = item['p'][0]['index']
+          target_index = item['p'][2]['index']
+                                   
+          net.add_node(source_index, color="#00FF7F")
+          net.add_node(target_index)
+          net.add_edge(source_index, target_index, label='PAYS',arrows='to') 
+               #     net.barnes_hut()
+     net.save_graph(str(settings.BASE_DIR)+'/lab/templates/pvis_graph_file.html')
+     number_of_nodes = c.count()[0]['COUNT(n)']
+     c.refresh_graph()
+     number_of_relationships = c.create_graph_catalogue()[0]['relationshipCount']
 
-# def map_data(data,request=None):
-#      net = Network(height="1500px",width="100%",bgcolor='#222222',font_color="white")
-#      for item in data:
-#                source_index = item['p'][0]['index']
-#                target_index = item['p'][2]['index']
-               
-#                net.add_node(source_index)
-#                net.add_node(target_index)
-#                net.add_edge(source_index, target_index, label='PAYS') 
-          
-#      html = net.generate_html(name="ss.html")
-#      return render(request,"lab/ss.html",{'html':html})
+     context = {'n':number_of_nodes,'r':number_of_relationships}
+     return render(request,"lab/wannacry/wannacry2.html",context=context)
 
      
+def page_rank_1(request):
+
+     pageRank = c.pageRank()
+     data = json.dumps(c.set_properties_for_degree_centrality())
+     context = {'data':data}
+
+     return render(request,"lab/wannacry/page-rank.html",context=context)
+
+def btc_txid(request):
+     data = json.dumps(c.total_amount_passing_tx_node())
+     context = {'data':data}
+     return render(request,'lab/wannacry/btc_txid.html',context=context)
+      
+
+
+def btc_address_node(request):
+     data = json.dumps(c.total_amount_passing_address_node())
+     context = {'data':data}
+     return render(request,'lab/wannacry/btc_address_node.html',context=context)
+
+def risk_rating_tx(request):
+     data = json.dumps(c.risk_rating_txt_node())
+     context = {'data':data}
+     return render(request,'lab/wannacry/risk_rating_tx.html',context=context)
+
+
+def risk_rating_address(request):
+     data = json.dumps(c.risk_rating_address_node())
+     context = {'data':data}
+     print(data)
+     return render(request,'lab/wannacry/risk_rating_address.html',context=context)
+
+def SageMaker(request):
+     c.delete_graph('addresses_with_transactions_1')
+     c.create_graph_catalog_for_graph_sage_model()
+     c.delete_model('weightedTrainedModel')
+     c.train_graph_sage_model()
+     c.delete_model('testModel')
+     c.test_different_hp_graph_sage()
+     data = c.FastRP()
+     context = {'data':data}
+     return render(request,'lab/wannacry/sagemaker.html',context=context)
 
 
 
-
-
-        #From -> item["p"][0]
-        #{'in_degree': 0, 'pageRank': 0.15000000000000002, 'depth': ['0'], 'risk_rating': 7.83608898204724, 'total_amount': 17.77113036999999, 'out_degree': 112, 'index': '12t9YDPgwueZ9NyMgw519p7AA8isjr6SMw', 'label': 'NA'}
-        
-        #relationship -> item["p"][1]
-        #    PAYS
-       
-        # To -> item["p"][2] 
-        # {'in_degree': 76, 'pageRank': 0.23651785714285709, 'depth': ['0'], 'time_stamp': '03/08/2017 04:41:34 UTC', 'risk_rating': 5.476845459488191, 'total_amount': 18.066477230000004, 'out_degree': 1, 'index': '35e5d5fe8c8128cfa6884f56be5817e4138c58c91b79d78d3e78a8d365b9d8a7'}
-
-
-
-
-
-
-
-
-
-
-#     print(data[0]["p"][0]["in_degree"])
-#     write_json(data,address)     
-
-
-
-# def write_json(data , addr):
-#     filename = addr+".json"
-#     with open(filename,"w") as f :
-#         json.dump(data,f,indent= 4)
