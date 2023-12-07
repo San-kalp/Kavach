5661f28cf1bd10140b3bcd9e9622bc4460a3cde1
Sankalp Chordia <84339542+San-kalp@users.noreply.github.com>
2023-12-07 19:57:49 +0530

    Update

--- Defendify-project/Cypher.py
+++ Defendify-project/Cypher.py
@@ -1,14 +1,6 @@
 from neo4j import GraphDatabase
 
-uri = "bolt://localhost:7687"
-user = "neo4j"
-password = "12345678"
-driver = GraphDatabase.driver(uri, auth=(user, password))
-session = driver.session(database="test")
 
-def handle(query):
-    result = session.run(query)
-    return result.data()
 
 
 # #Will delete all nodes that have relationship
